94edfc553ae9ecd4760e99eb38b6c2b6c78bfd6e
ioncarrasco <crackcatacrak@gmail.com>
2025-01-27 17:58:47 +0100

    some updates in location

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,11 @@
 let mapOptions = {
     center: [39.464796423540136, -0.36799903816171786],
-    zoom: 10
+    zoom: 20
 }
 
 let map = new L.map('map', mapOptions)
 
-let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+let layer = new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
 
 map.addLayer(layer)
 
